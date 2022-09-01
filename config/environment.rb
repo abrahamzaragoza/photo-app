32c39857e6b5de62fafef975fa1d5cbb565c0ddb
abrahamzaragoza <abraham.zaragoza@enroutesystems.com>
2022-09-01 16:41:55 -0500

    Add Mailgun integration to Heroku - RoR project

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => '587',
+  :address        => 'smtp.mailgun.org',
+  :user_name      => 'postmaster@sandboxfde4401441954138ad501881d1fdfacf.mailgun.org',
+  :password       => '5f18672cb38dc21f964b3aae7ad29e1c-07e2c238-5615a6a5',
+  :domain         => 'https://btt-photo-app-z.herokuapp.com/',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  onfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {
+    :host => 'https://btt-photo-app-z.herokuapp.com/',
+    :protocol => 'https'
+  }
 end
