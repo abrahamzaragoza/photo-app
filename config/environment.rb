310ce882d9eebb567af9d3dd012bb1dfebc591d6
abrahamzaragoza <abraham.zaragoza@enroutesystems.com>
2022-09-02 11:35:11 -0500

    Add new credentials

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,8 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :port           => '587',
   :address        => 'smtp.mailgun.org',
-  :user_name      => 'postmaster@sandboxfde4401441954138ad501881d1fdfacf.mailgun.org',
-  :password       => '5f18672cb38dc21f964b3aae7ad29e1c-07e2c238-5615a6a5',
+  :user_name      => ENV['MAILGUN_USERNAME'],
+  :password       => ENV['MAILGUN_PASSWORD'],
   :domain         => 'https://btt-photo-app-z.herokuapp.com/',
   :authentication => :plain,
 }
